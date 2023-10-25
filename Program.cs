4bb6b46e4096cca61a2ebac17a43fb371781a15c
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-10-25 09:11:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak25._10._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("upusite recenicu");
+            string s1 = Console.ReadLine();
+            string[] rijeci = s1.Split(' ');
+            Console.WriteLine(rijeci[0]);
+            Console.WriteLine(rijeci[rijeci.Length-1]);
+            Console.ReadKey();
+        }
+    }
+}
